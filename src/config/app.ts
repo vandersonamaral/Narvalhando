aa78928fa88270d59d84a1be563e77044b2a23c1
vandersonamaral <vandersonamaral27@gmail.com>
2025-11-26 14:20:10 -0300

    feat: register JWT plugin, auth middleware and auth routes in app config

--- src/config/app.ts
+++ src/config/app.ts
@@ -5,12 +5,24 @@ import {
   serializerCompiler,
   ZodTypeProvider,
 } from "fastify-type-provider-zod";
+import jwt from "@fastify/jwt";
+
+import {registerRoutes} from "../auth/register";
+import {authMiddleware} from "../middleware/auth";
+
+
 
 const app = fastify().withTypeProvider<ZodTypeProvider>();
 
+app.register(jwt, {
+  secret: process.env.JWT_SECRET || "secret-super-seguro",
+});
+app.register(authMiddleware);
 app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
 app.register(fastifyCors, {
   origin: "*",
 });
+app.register(registerRoutes);
+
 export default app;
